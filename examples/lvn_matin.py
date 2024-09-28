3ecc2db8cce93a70852d6e7a03474a0f3053690e
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-09-28 19:17:50 -0400

    Ensure commutative property is enforced when hashing the expressions.

--- examples/lvn_matin.py
+++ examples/lvn_matin.py
@@ -1,10 +1,15 @@
 import json
 import sys
 
-from numpy import number
-
 from form_blocks import form_blocks
 from util import flatten
+
+
+def is_commutative(op):
+    """
+    Returns true whether the op's args can be sorted or not.
+    """
+    return op in {"add", "mul", "eq", "fadd", "fmul", "feq", "and", "or"}
 
 
 class ValueNumberTable:
@@ -28,7 +33,6 @@ class ValueNumberTable:
         return var_name
 
     def get_canonical_of_value(self, val):
-        # print("canonical: ", val, self.value_to_canonical_variable_map[val])
         return self.value_to_canonical_variable_map[val]
 
     def get_or_create_use_value(self, variable):
@@ -36,14 +40,10 @@ class ValueNumberTable:
         if variable not in self.num_defs_per_variable:
             self.num_defs_per_variable[variable] = 0
         var_name = self._get_renamed_variable_name(variable)
-        # print(self.variable_to_value_map)
         if var_name not in self.variable_to_value_map:
-            # print("creating variable", var_name)
             self.variable_to_value_map[var_name] = self.next_value
             self.value_to_canonical_variable_map[self.next_value] = var_name
             self.next_value += 1
-        # print("get or create use res: ", variable, var_name, self.variable_to_value_map[var_name])
-        # print(self.value_to_canonical_variable_map[self.next_value - 1])
         return self.variable_to_value_map[var_name]
 
     def get_def_value(self, instr):
@@ -73,7 +73,6 @@ class ValueNumberTable:
             self.num_defs_per_variable[dest_variable] += 1
             # Add the renamed variable name to the mapping between variable names and values
             dest_var_name = self._get_renamed_variable_name(dest_variable)
-            # print("creating def variable", dest_var_name, self.next_value)
             self.variable_to_value_map[dest_var_name] = self.next_value
             # Lookup the expression of this instruction defining the dest; If it's already
             # there, return the value it is assigned to; Otherwise, create a new value for the def
@@ -81,20 +80,25 @@ class ValueNumberTable:
                 self.value_to_canonical_variable_map[self.next_value] = dest_var_name
                 self.expr_to_value_map[expr] = self.next_value
                 self.next_value += 1
-            # print("Create def res:", dest_variable, expr, dest_var_name, self.expr_to_value_map[expr])
             return self.expr_to_value_map[expr]
         else:
             return None
 
     def get_expr_of_instr(self, inst):
         expr = []
+        is_comm = False
         if "op" in inst:
             expr.append(inst["op"])
+            is_comm = is_commutative(inst["op"])
             if inst["op"] == "const":
                 expr.append(inst["value"])
+        args = []
         if "args" in inst:
             for arg in inst["args"]:
-                expr.append(self.get_or_create_use_value(arg))
+                args.append(self.get_or_create_use_value(arg))
+        if is_comm:
+            args = sorted(args)
+        expr += args
         return tuple(expr)
 
 
