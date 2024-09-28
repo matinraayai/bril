d1b0f5405edd52624ed4d114f78561ab90043f64
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-09-28 18:41:44 -0400

    Some bug fixes.

--- examples/lvn_matin.py
+++ examples/lvn_matin.py
@@ -23,11 +23,12 @@ class ValueNumberTable:
     def _get_renamed_variable_name(self, variable):
         var_name = variable
         # Append a number to the variable name in case it is
-        if variable in self.num_defs_per_variable and self.num_defs_per_variable[variable] != 0:
+        if variable in self.num_defs_per_variable and self.num_defs_per_variable[variable] > 1:
             var_name += str(self.num_defs_per_variable[variable])
         return var_name
 
     def get_canonical_of_value(self, val):
+        # print("canonical: ", val, self.value_to_canonical_variable_map[val])
         return self.value_to_canonical_variable_map[val]
 
     def get_or_create_use_value(self, variable):
@@ -35,18 +36,27 @@ class ValueNumberTable:
         if variable not in self.num_defs_per_variable:
             self.num_defs_per_variable[variable] = 0
         var_name = self._get_renamed_variable_name(variable)
+        # print(self.variable_to_value_map)
         if var_name not in self.variable_to_value_map:
+            # print("creating variable", var_name)
             self.variable_to_value_map[var_name] = self.next_value
             self.value_to_canonical_variable_map[self.next_value] = var_name
             self.next_value += 1
+        # print("get or create use res: ", variable, var_name, self.variable_to_value_map[var_name])
+        # print(self.value_to_canonical_variable_map[self.next_value - 1])
         return self.variable_to_value_map[var_name]
 
     def get_def_value(self, instr):
         if "dest" in instr:
+            dest_var = instr["dest"]
             expr = self.get_expr_of_instr(instr)
             # Lookup the expression of this instruction defining the dest; If it's already
             # there, return the value it is assigned to; Otherwise, create a new value for the def
             if expr in self.expr_to_value_map:
+                renamed_dest_var = self._get_renamed_variable_name(dest_var)
+                # Also record the def value if not already in the variable map
+                if renamed_dest_var not in self.variable_to_value_map:
+                    self.variable_to_value_map[renamed_dest_var] = self.expr_to_value_map[expr]
                 return self.expr_to_value_map[expr]
             else:
                 return None
@@ -63,6 +73,7 @@ class ValueNumberTable:
             self.num_defs_per_variable[dest_variable] += 1
             # Add the renamed variable name to the mapping between variable names and values
             dest_var_name = self._get_renamed_variable_name(dest_variable)
+            # print("creating def variable", dest_var_name, self.next_value)
             self.variable_to_value_map[dest_var_name] = self.next_value
             # Lookup the expression of this instruction defining the dest; If it's already
             # there, return the value it is assigned to; Otherwise, create a new value for the def
@@ -70,6 +81,7 @@ class ValueNumberTable:
                 self.value_to_canonical_variable_map[self.next_value] = dest_var_name
                 self.expr_to_value_map[expr] = self.next_value
                 self.next_value += 1
+            # print("Create def res:", dest_variable, expr, dest_var_name, self.expr_to_value_map[expr])
             return self.expr_to_value_map[expr]
         else:
             return None
@@ -77,12 +89,12 @@ class ValueNumberTable:
     def get_expr_of_instr(self, inst):
         expr = []
         if "op" in inst:
-            expr += inst["op"]
+            expr.append(inst["op"])
             if inst["op"] == "const":
-                expr += inst["value"]
+                expr.append(inst["value"])
         if "args" in inst:
             for arg in inst["args"]:
-                expr += self.get_or_create_use_value(arg)
+                expr.append(self.get_or_create_use_value(arg))
         return tuple(expr)
 
 
@@ -122,21 +134,23 @@ def lvn_block_pass(block):
         dest_val = None
         if "args" in instr:
             for arg in instr["args"]:
-                arg_vals += number_table.get_or_create_def_value(arg)
+                arg_vals.append(number_table.get_or_create_use_value(arg))
 
         dest_val = number_table.get_def_value(instr)
         # We don't have a value defined for this def; Create one.
         if "dest" in instr and dest_val is None:
             dest_val = number_table.get_or_create_def_value(instr)
-
+        # print(instr, arg_vals, dest_val)
         # Re-materialize the instruction by renaming its operands and dest
         if len(arg_vals) != 0:
             for i, arg_val in enumerate(arg_vals):
                 instr["args"][i] = number_table.get_canonical_of_value(arg_val)
         if "dest" in instr:
+            # print("Is live out def: ")
             # Don't change the variable name if it is a live-out
-            if not is_live_out_def:
-                instr["dest"] = number_table.get_canonical_of_value(dest_val)
+            # if not is_live_out_def:
+            instr["dest"] = number_table.get_canonical_of_value(dest_val)
+        # print(instr)
 
 
 if __name__ == '__main__':
