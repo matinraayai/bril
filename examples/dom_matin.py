448c9bc15e1048ef1b13a1abc156090c35c5be36
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-12-13 00:33:18 -0500

    Minor bug fix.

--- examples/dom_matin.py
+++ examples/dom_matin.py
@@ -4,6 +4,7 @@ import sys
 from cfg import block_map, edges, add_terminators, add_entry
 from form_blocks import form_blocks
 
+
 def calculate_dom(func):
     # Get the blocks, and ensure it has an entry block, and add terminators at the end of each block
     blocks = block_map(form_blocks(func['instrs']))
@@ -12,7 +13,7 @@ def calculate_dom(func):
     # Get the predecessors graph
     predecessor_map, _ = edges(blocks)
     # Initially, map all blocks to each other
-    dom_set = {block : {b for b in blocks.items()} for block in blocks.items()}
+    dom_set = {block: {b for b in blocks} for block in blocks}
     # Remove the entry block from the iteration, as we know it dominates everything
     blocks_without_entry = {name: b for (name, b) in blocks.items() if name != list(blocks.keys())[0]}
     changed = True
@@ -21,7 +22,7 @@ def calculate_dom(func):
         for block in blocks_without_entry:
             current_dom = set()
             # dom[vertex] = {vertex} ∪ ⋂{dom[p] for p in vertex.preds}
-            current_dom.intersection_update(dom_set[p] for p in predecessor_map[block])
+            current_dom.intersection_update(*[dom_set[p] for p in predecessor_map[block]])
             current_dom.add(block)
 
             # Replace the entry if it has changed
@@ -30,6 +31,7 @@ def calculate_dom(func):
                 changed = True
 
     return dom_set
+
 
 if __name__ == '__main__':
     module = json.load(sys.stdin)
