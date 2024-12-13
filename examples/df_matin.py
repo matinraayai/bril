93d3bfd3f8bd2f7099fb4c8cbb3b74b909063e52
matinraayai <30674652+matinraayai@users.noreply.github.com>
2024-12-12 21:28:06 -0500

    Debugged df_matin.py

--- examples/df_matin.py
+++ examples/df_matin.py
@@ -45,6 +45,7 @@ def get_uses_with_no_defs(block):
             defs.add(i['dest'])
     return out
 
+
 def liveness_analysis(module):
     """
     Performs a liveness analysis using the work list algorithm
@@ -59,29 +60,31 @@ def liveness_analysis(module):
         # Initialize the work list algorithm variables
         # The work list is a FIFO queue here
         work_list = list(blocks.keys())
-        live_outs = {node: set() for node in blocks}
-        live_ins = {node: set() for node in blocks}
+        live_outs = {block_name: set() for block_name in blocks}
+        live_ins = {block_name: set() for block_name in blocks}
 
         # Start from the exit block, and traverse the CFG, updating the work list in case the out set of the
         # block has changed
         # The algorithm will stop when all the blocks don't get updated
         while len(work_list) != 0:
-            block = work_list.pop(0)
+            block_name = work_list.pop(0)
             # Update the live-outs set using the meet (merge) operator
             # The union of the live-ins of all successor blocks is set to be the live-outs
             # of the current block
-            live_outs[block].union(live_ins[n] for n in successor_map[block])
+            live_outs[block_name].update(*[live_ins[n] for n in successor_map[block_name]])
             # Update the live-ins set using the transfer operator
-            current_live_ins = get_uses_with_no_defs(block).union(live_outs[block] - get_defs(block))
+            current_live_ins = get_uses_with_no_defs(blocks[block_name]).union(
+                live_outs[block_name] - get_defs(blocks[block_name]))
 
-            if current_live_ins != live_ins[block]:
-                live_ins[block] = current_live_ins
-                work_list += predecessor_map[block]
+            if current_live_ins != live_ins[block_name]:
+                live_ins[block_name] = current_live_ins
+                work_list += predecessor_map[block_name]
 
-        for block in blocks:
-            print('{}:'.format(block))
-            print('  in: ', fmt(live_ins[block]))
-            print('  out:', fmt(live_outs[block]))
+        for block_name in blocks:
+            print('{}:'.format(block_name))
+            print('  in: ', fmt(live_ins[block_name]))
+            print('  out:', fmt(live_outs[block_name]))
+
 
 if __name__ == '__main__':
     liveness_analysis(json.load(sys.stdin))
